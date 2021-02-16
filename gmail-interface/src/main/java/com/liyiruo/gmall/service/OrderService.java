4650218fa675a94243dc3c42afa0ea14e28ee0fb
liyiruo <401213245@qq.com>
2021-02-16 10:41:20 +0800

    全部提交

--- gmail-interface/src/main/java/com/liyiruo/gmall/service/OrderService.java
+++ gmail-interface/src/main/java/com/liyiruo/gmall/service/OrderService.java
@@ -1,10 +1,14 @@
 package com.liyiruo.gmall.service;
 
+import com.liyiruo.gmall.bean.UserAddress;
+
+import java.util.List;
+
 /**
  * @author liyiruo
  * @Description
  * @Date 2021/2/13 下午11:14
  */
 public interface OrderService {
-    void initOrder(String userId);
+    List<UserAddress> initOrder(String userId);
 }
